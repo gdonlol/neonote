cfae8531cf0bf353a7425c4027f15eb565dc1b73
gdonlol <gordon.liang.xu@gmail.com>
2025-03-23 00:05:50 -0400

    progress

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,17 @@ void render_menu()
 {
   // box(win, 0, 0); //this for border if we want
   mvwprintw(win, (LINES - 10) / 2, (COLS - 8) / 2, "neonote.");
-  mvwprintw(win, (LINES - 2) / 2, (COLS - 16) / 2, "1. my notes");
-  mvwprintw(win, (LINES - 1) / 2, (COLS - 16) / 2, "2. exit");
+
+  if (menu_option == 0)
+  {
+    mvwprintw(win, (LINES - 2) / 2, (COLS - 16) / 2, ">> my notes");
+    mvwprintw(win, (LINES - 1) / 2, (COLS - 16) / 2, "2. exit");
+  }
+  else
+  {
+    mvwprintw(win, (LINES - 2) / 2, (COLS - 16) / 2, "1. my notes");
+    mvwprintw(win, (LINES - 1) / 2, (COLS - 16) / 2, ">> exit");
+  }
 
   refresh();
 }
