2badb5382a033156ad3fa6142ba66f74072165e5
gdonlol <gordon.liang.xu@gmail.com>
2025-03-23 14:09:46 -0400

    small changes

--- src/render/RenderEditor.cpp
+++ src/render/RenderEditor.cpp
@@ -6,17 +6,17 @@ void renderEditor(WINDOW *win)
     werase(win);
     wrefresh(win);
 
-    int sidebar_width = COLS * 0.3;
+    int sidebar_width = COLS * 0.25;
     int content_width = COLS - sidebar_width;
-    WINDOW *sidebar = derwin(win, LINES - 2, sidebar_width - 2, 1, 1);
-    WINDOW *content = derwin(win, LINES - 2, content_width - 2, 1, sidebar_width);
+    WINDOW *sidebar = derwin(win, LINES, sidebar_width, 0, 0);
+    WINDOW *content = derwin(win, LINES, content_width, 0, sidebar_width);
     refresh();
 
     box(sidebar, 0, 0);
-    mvwprintw(sidebar, 1, 1, "Menu");
+    mvwprintw(sidebar, 1, 2, "Menu");
 
     box(content, 0, 0);
-    mvwprintw(content, 1, 1, "Content");
+    mvwprintw(content, 1, 2, "Content");
 
     wrefresh(win);
     wrefresh(sidebar);
