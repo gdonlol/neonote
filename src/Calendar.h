8b6c9920ea62ad6279a711462ac5f2b1d121a8f4
gxu94 <gxu94@uwo.ca>
2025-03-30 22:00:54 -0400

    sadasdsa :

--- src/Calendar.h
+++ src/Calendar.h
@@ -8,23 +8,29 @@
 
 class Calendar {
 public:
-        Calendar(WINDOW *content);
-        void addEvent(const Event& event);
-        void removeEvent(int eventId);
-        void updateEvent(int eventId, Event& updatedEvent);
+    Calendar(WINDOW *content);
+    void addEvent(const Event& event);
+    void removeEvent(int eventId);
+    void updateEvent(int eventId, Event& updatedEvent);
+    std::vector<Event> getEvents();
 
-        int getCurrentDay() const;
-        int getCurrentMonth() const;
-        int getCurrentYear() const;
-        int getDaysInMonth(int month, int year) const;
-        int getFirstDayOfMonth(int month, int year) const;
+    int getCurrentDay() const;
+    int getCurrentMonth() const;
+    int getCurrentYear() const;
+    int getDaysInMonth(int month, int year) const;
+    int getFirstDayOfMonth(int month, int year) const;
 
-        void renderCalendar();
-        int nextFree();
+    void renderCalendar();
+    int nextFree();
+
+    void setSelectedEvent(int index);
+    int getSelectedEvent();
     
 private:
     std::vector<Event> events;
     WINDOW *content;
+
+    int selectedEvent;
 };
 
 #endif // CALENDAR_H
